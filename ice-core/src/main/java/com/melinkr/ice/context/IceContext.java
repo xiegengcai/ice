b584719cf10c804d2c74b96ad6bec5e4afbf918b
風一樣的男子 <xiegengcai@foxmail.com>
2016-09-13 11:46:31 +0800

    修改IceContext实现方式

--- ice-core/src/main/java/com/melinkr/ice/context/IceContext.java
+++ ice-core/src/main/java/com/melinkr/ice/context/IceContext.java
@@ -10,14 +10,13 @@ import com.melinkr.ice.utils.IceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,84 +25,67 @@ import java.util.Map;
  * Created by <a href="mailto:xiegengcai@gmail.com">Xie Gengcai</a> on 2016/8/20.
  */
 @Component
-public class IceContext implements ApplicationContextAware {
+public class IceContext implements BeanPostProcessor {
     protected Logger logger = LoggerFactory.getLogger(IceContext.class);
-    //    / Spring应用上下文环境
-    private static ApplicationContext context;
     private Map<String, ServiceMethodHandler> serviceHandlerMap = new HashMap<>();
 
+
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        context = applicationContext;
-    }
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (AnnotationUtils.findAnnotation(bean.getClass(), WebService.class) != null) {
+            ReflectionUtils.doWithMethods(bean.getClass(),
+                    method -> {
+                        //webservice 方法的注解
+                        ServiceMethod serviceMethod = AnnotationUtils.findAnnotation(method, ServiceMethod.class);
+                        //方法注解上的值
+                        ServiceMethodValue serviceMethodValue = IceBuilder.buildServiceMethodValue(serviceMethod);
+                        //处理方法的类
+                        ServiceMethodHandler serviceMethodHandler = new ServiceMethodHandler();
+                        //serviceMethodValue
+                        serviceMethodHandler.setServiceMethodValue(serviceMethodValue);
+                        //handler
+                        serviceMethodHandler.setHandler(bean);
+                        //method
+                        serviceMethodHandler.setHandlerMethod(method);
 
-    /**
-     * 初始化注解等
-     */
-    @PostConstruct
-    public void init() {
-        logger.info("对Spring上下文中的Bean进行扫描，注册ice服务方法: {}", context);
-        String[] beanNames = context.getBeanNamesForType(Object.class);
-        for (final String beanName : beanNames) {
-            Class<?> handlerType = context.getType(beanName);
-            if (AnnotationUtils.findAnnotation(handlerType, WebService.class) != null) {
-                ReflectionUtils.doWithMethods(handlerType,
-                        method -> {
-                            //webservice 方法的注解
-                            ServiceMethod serviceMethod = AnnotationUtils.findAnnotation(method, ServiceMethod.class);
-                            //方法注解上的值
-                            ServiceMethodValue serviceMethodValue = IceBuilder.buildServiceMethodValue(serviceMethod);
-                            //处理方法的类
-                            ServiceMethodHandler serviceMethodHandler = new ServiceMethodHandler();
-                            //serviceMethodValue
-                            serviceMethodHandler.setServiceMethodValue(serviceMethodValue);
-                            //handler
-                            serviceMethodHandler.setHandler(context.getBean(beanName));
-                            //method
-                            serviceMethodHandler.setHandlerMethod(method);
-
-                            if (method.getParameterTypes().length > 1) {
-                                logger.error("参数必须是一个参数或者无参数");
-                                throw new IceException("参数必须是一个参数或者无参数");
-                            } else if (method.getParameterTypes().length == 1) {
-                                Class<?> paramType = method.getParameterTypes()[0];
-                                if (ClassUtils.isAssignable(IceRequest.class, paramType)) {
-                                    @SuppressWarnings("unchecked")
-                                    Class<? extends IceRequest> classType = (Class<? extends IceRequest>)paramType;
-                                    serviceMethodHandler.setRequestType(classType);
-                                    //忽略签名的列表
-                                    List<String> ignoreSignFieldNames = IceBuilder.buildNoSignFieldNames(classType);
-                                    serviceMethodHandler.setIgnoreSignFieldNames(ignoreSignFieldNames);
-                                }else{
-                                    logger.error("参数必须继承IceRequest");
-                                    throw new IceException("参数必须继承IceRequest");
-                                }
+                        if (method.getParameterTypes().length > 1) {
+                            logger.error("参数必须是一个参数或者无参数");
+                            throw new IceException("参数必须是一个参数或者无参数");
+                        } else if (method.getParameterTypes().length == 1) {
+                            Class<?> paramType = method.getParameterTypes()[0];
+                            if (ClassUtils.isAssignable(IceRequest.class, paramType)) {
+                                @SuppressWarnings("unchecked")
+                                Class<? extends IceRequest> classType = (Class<? extends IceRequest>)paramType;
+                                serviceMethodHandler.setRequestType(classType);
+                                //忽略签名的列表
+                                List<String> ignoreSignFieldNames = IceBuilder.buildNoSignFieldNames(classType);
+                                serviceMethodHandler.setIgnoreSignFieldNames(ignoreSignFieldNames);
+                            }else{
+                                logger.error("参数必须继承IceRequest");
+                                throw new IceException("参数必须继承IceRequest");
                             }
-                            String serviceMethodKey = new StringBuilder(serviceMethodValue.getMethod()).append("#").append(serviceMethodValue.getVersion()).toString();
-                            //存储
-                            ServiceMethodHandler oldHandler = serviceHandlerMap.put(serviceMethodKey, serviceMethodHandler);
-                            if(oldHandler != null){
-                                logger.error("定义了重复的方法名+版本");
-                                throw new IceException("定义了重复的方法名+版本数");
-                            }
-                            logger.info("注册ice服务{}", serviceMethodKey);
-                        },
-                        method -> !method.isSynthetic() && AnnotationUtils.findAnnotation(method, ServiceMethod.class) != null
-                );
-            }
+                        }
+                        String serviceMethodKey = new StringBuilder(serviceMethodValue.getMethod()).append("#").append(serviceMethodValue.getVersion()).toString();
+                        //存储
+                        ServiceMethodHandler oldHandler = serviceHandlerMap.put(serviceMethodKey, serviceMethodHandler);
+                        if(oldHandler != null){
+                            logger.error("定义了重复的方法名+版本");
+                            throw new IceException("定义了重复的方法名+版本数");
+                        }
+                        logger.info("注册ice服务{}", serviceMethodKey);
+                    },
+                    method -> !method.isSynthetic() && AnnotationUtils.findAnnotation(method, ServiceMethod.class) != null
+            );
         }
-        logger.info("注册ice服务完成");
+        return bean;
     }
 
-    public static Object getBean(String beanName) {
-        return context.getBean(beanName);
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
     }
 
-    public static <T>T getBean(String beanName , Class<T>clazz) {
-        return context.getBean(beanName , clazz);
-    }
-
-    public ServiceMethodHandler getServiceMethodHandler(String method,String version){
+    public ServiceMethodHandler getServiceMethodHandler(String method, String version){
         return serviceHandlerMap.get(method+"#"+version);
     }
 }
